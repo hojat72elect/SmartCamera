c678c877f08aae677222e03e5aee9ea6c3924f5f
hojat <hojat72elect@gmail.com>
2023-08-19 11:36:10 -0400

    Implemented a basic camera preview and the logic for asking for camera permissions.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,68 @@
-import {Text, View} from 'react-native';
+import {Alert, Text, TouchableOpacity, View} from 'react-native';
+import {useState} from "react";
+import {Camera, CameraType, PermissionStatus} from "expo-camera";
+import {StatusBar} from "expo-status-bar";
 
 export default function App() {
 
+    let camera: Camera | null;
+    const [startCamera, setStartCamera] = useState(false);
 
-    return (
+    const __startCamera = async () => {
+        const {status} = await Camera.requestCameraPermissionsAsync();
+        if (status === PermissionStatus.GRANTED) {
+            setStartCamera(true);
+        } else {
+            Alert.alert("Access denied");
+        }
+    }
+
+    /**
+     *
+     */
+
+    return (startCamera ? (<Camera
+        style={{flex: 1, width: '100%'}}
+        ref={(r) => {
+            camera = r;
+        }}
+        type={CameraType.front}
+    ></Camera>) : (
         <View style={{
             flex: 1,
             backgroundColor: '#fff',
-            justifyContent: 'center',
             alignItems: 'center',
+            justifyContent: 'center',
         }}>
-            <Text>Hello world</Text>
+            <View style={{
+                flex: 1,
+                backgroundColor: '#fff',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <TouchableOpacity style={{
+                    width: 130,
+                    borderRadius: 4,
+                    backgroundColor: '#14274e',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: 40,
+                }}
+                                  onPress={__startCamera}
+                >
+                    <Text
+                        style={{
+                            color: '#fff',
+                            fontWeight: 'bold',
+                            textAlign: 'center',
+                        }}>
+                        Take Picture
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <StatusBar style="auto"/>
         </View>
-    );
+    ));
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is the latest of multiplatform mobile apps that I have developed and published to the google play store.
+
+
+I am following [this article](https://www.freecodecamp.org/news/how-to-create-a-camera-app-with-expo-and-react-native/) and have made a simple screen with button so far.
