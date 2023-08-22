f1ceecf79f3aba6eb779abe4f85e5edcee85fe56
hojat <hojat72elect@gmail.com>
2023-08-21 22:35:20 -0400

    Extracted CapturedPhotoPreview.tsx into its own component.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import {StatusBar} from 'expo-status-bar'
 import {useState} from 'react'
-import {Alert, ImageBackground, Text, TouchableOpacity, View} from 'react-native'
+import {Alert, Text, TouchableOpacity, View} from 'react-native'
 import {Camera, CameraCapturedPicture, CameraType, FlashMode, PermissionStatus} from 'expo-camera'
 import {RootSiblingParent} from 'react-native-root-siblings';
 import Toast from 'react-native-root-toast';
+import {CapturedPhotoPreview} from "./src/ui/CapturedPhotoPreview";
 
 export default function App() {
 
@@ -214,84 +215,4 @@ export default function App() {
             </View>
         </RootSiblingParent>
     );
-}
-
-type CapturedPhotoPreviewProps = {
-    photo: CameraCapturedPicture;
-    retakePicture: () => void;
-    savePhoto: () => void;
-};
-
-const CapturedPhotoPreview = ({photo, retakePicture, savePhoto}: CapturedPhotoPreviewProps) => {
-    console.log('the captured photo is this:', photo);
-    return (
-        <View
-            style={{
-                backgroundColor: 'transparent',
-                flex: 1,
-                width: '100%',
-                height: '100%'
-            }}
-        >
-            <ImageBackground
-                source={{uri: photo && photo.uri}}
-                style={{
-                    flex: 1
-                }}
-            >
-                <View
-                    style={{
-                        flex: 1,
-                        flexDirection: 'column',
-                        padding: 15,
-                        justifyContent: 'flex-end'
-                    }}
-                >
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between'
-                        }}
-                    >
-                        <TouchableOpacity
-                            onPress={retakePicture}
-                            style={{
-                                width: 130,
-                                height: 40,
-                                alignItems: 'center',
-                                borderRadius: 4
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    color: 'black',
-                                    fontSize: 20
-                                }}
-                            >
-                                Re-take
-                            </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={savePhoto}
-                            style={{
-                                width: 130,
-                                height: 40,
-                                alignItems: 'center',
-                                borderRadius: 4
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    color: 'black',
-                                    fontSize: 20
-                                }}
-                            >
-                                Save Photo
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </ImageBackground>
-        </View>
-    )
 }
---
+++ src/ui/CapturedPhotoPreview.tsx
@@ -0,0 +1,87 @@
+import {ImageBackground, Text, TouchableOpacity, View} from "react-native";
+import {CameraCapturedPicture} from "expo-camera";
+
+type CapturedPhotoPreviewProps = {
+    photo: CameraCapturedPicture;
+    retakePicture: () => void;
+    savePhoto: () => void;
+};
+
+/**
+ * When user takes a picture, they will see a preview of
+ * it in this page and will be allowed to either save or
+ * re-take the picture.
+ */
+export const CapturedPhotoPreview = ({photo, retakePicture, savePhoto}: CapturedPhotoPreviewProps) => {
+    console.log('the captured photo is this:', photo);
+    return (
+        <View
+            style={{
+                backgroundColor: 'transparent',
+                flex: 1,
+                width: '100%',
+                height: '100%'
+            }}
+        >
+            <ImageBackground
+                source={{uri: photo && photo.uri}}
+                style={{
+                    flex: 1
+                }}
+            >
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        padding: 15,
+                        justifyContent: 'flex-end'
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <TouchableOpacity
+                            onPress={retakePicture}
+                            style={{
+                                width: 130,
+                                height: 40,
+                                alignItems: 'center',
+                                borderRadius: 4
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    color: 'black',
+                                    fontSize: 20
+                                }}
+                            >
+                                Re-take
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={savePhoto}
+                            style={{
+                                width: 130,
+                                height: 40,
+                                alignItems: 'center',
+                                borderRadius: 4
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    color: 'black',
+                                    fontSize: 20
+                                }}
+                            >
+                                Save Photo
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ImageBackground>
+        </View>
+    )
+}
